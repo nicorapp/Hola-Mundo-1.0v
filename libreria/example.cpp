ee17c294eff934fce010e48d1b4ba86e782d7a88
Nicolás Rapp <nicolas.rapp.ar@gmail.com>
2022-09-26 20:03:32 -0300

    Update example.cpp

--- libreria/example.cpp
+++ libreria/example.cpp
@@ -1,9 +1,27 @@
 #include "example.h"
+#include <iostream>
+#include <string>
+#include "math.h"
+using namespace std;
 
 namespace foobar
 {
     int Example::getValue() const
     {
-        int a;
+        double a, b, c, r1, r2;
+        cout << "Introduzca un valor para a: ";
+        cin >> a;
+        cout << "Introduzca un valor para b: ";
+        cin >> b;
+        cout << "Introduzca un valor para c: ";
+        cin >> c;
+        r1 = (-b + sqrt((b * b) - (4 * a * c))) / (2 * a);
+        r2= (-b + sqrt((b * b) - (4 * a * c))) / (2 * a);
+        if (((b * b) - (4 * a * c)) <= 0)
+        {
+            cout << "ERROR!";
+        }
+        cout << "Las raices son: " << r1 << " y " << r2;
+        return 0;
     }
 }
