189f2c563ed7253f78223917d6a3be6cacc6cf18
Tacostefano <tacostefano@gmail.com>
2022-09-27 22:43:48 -0300

    test

--- unit-test/Testeos.cpp
+++ unit-test/Testeos.cpp
@@ -13,3 +13,11 @@ TEST(funcion, buscar_ceros)
 	ASSERT_DOUBLE_EQ(raiz_1,2);
 	ASSERT_DOUBLE_EQ(raiz_2,-2);
 }
+
+TEST(funcion, buscar_distancia) {
+	int x1 = 2, y1 = 3, z1 = 4, x2 = 5, y2 = 6, z2 = 7;
+	float distance;
+	distance = distancia(x1, y1, z1, x2, y2, z2);
+
+	ASSERT_FLOAT_EQ(distance, 11.78);
+}
