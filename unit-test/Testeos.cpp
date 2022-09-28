844dd5aa0485929a9455fc9fc1bd9862c700d814
MartinScorza <scorzamartin@gmail.com>
2022-09-27 22:30:03 -0300

    testeo

--- unit-test/Testeos.cpp
+++ unit-test/Testeos.cpp
@@ -3,3 +3,13 @@
 #include "Prototipos.h"
 
 using namespace testing;
+
+TEST(funcion, buscar_ceros)
+{
+	double raiz_1 = 6,raiz_2= 3;
+
+	resolvente(4, 0,raiz_1,raiz_2);
+
+	ASSERT_DOUBLE_EQ(raiz_1,2);
+	ASSERT_DOUBLE_EQ(raiz_2,-2);
+}
